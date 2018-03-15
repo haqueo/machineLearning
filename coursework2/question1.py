91db6e06714fbd2b95c2743f7047f691b99d256d
haqueo <omarhaque@hotmail.co.uk>
2018-03-15 05:14:36 +0000

    almost finished question1

--- coursework2/question1.py
+++ coursework2/question1.py
@@ -120,15 +120,22 @@ def maximisation_step(X_cleaned,expectations,K=10):
     return params
 
 
-def run_GMM(X_cleaned,K=10,params = -1,seed=1):
+def run_GMM(X_cleaned,K=10,params = -1,seed=1,maxiter=40):
     
     if (params == -1):
         random.seed(seed)
         params = initialise_parameters(X_cleaned,K)
     
-    for i in range(30):
+    for i in range(maxiter):
+        
+        old_params = params
+        
         expectations = compute_expectations(X_cleaned,params,K)
         params = maximisation_step(X_cleaned,expectations,K)
+        
+        if (np.allclose(params["means"],old_params["means"],rtol=1e-3)):
+            print("converged at iteration " + str((i+1)))
+            break
     
     return params, expectations
 
@@ -191,22 +198,45 @@ def count_cells(cleaned_dataset, cell_lower_bound, cell_upper_bound):
     AIC_list = {}
     BIC_list = {}
     
+    
+    # initialise using kmeans
+    centroids, data_clustering = k_means_clustering(cleaned_dataset,K=cell_lower_bound)
+    # initialise the arrays to be returned
+    means = centroids
+    covariances = np.zeros((K,dim,dim)) 
+    mixtures = np.zeros(K)
+
+    
+    # go through this initial clustering and initialise the parameters
+    for i in range(K):
+        X_k = X_cleaned[data_clustering == i,:]
+        covariances[i,:,:] = np.cov(X_k.transpose())
+        mixtures[i] = X_k.shape[0]/n
+    
+    
+    parameters["means"] = means
+    parameters["covariances"] = covariances
+    parameters["mixtures"] = mixtures
+    
+    
     for k_num in range(cell_lower_bound,cell_upper_bound+1):
         
-        params, expectations = run_GMM(cleaned_dataset,K=k_num)
+
+        params, expectations = run_GMM(cleaned_dataset,K=k_num,params=parameters)
         log_lik = log_likelihood(cleaned_dataset,k_num,params,expectations)
         # compute AIC
         AIC_list[k_num] = -2*log_lik + 2 * k_num
         # compute BIC
         BIC_list[k_num] = -2*log_lik + k_num * np.log(n)
         
-
+        parameters = params
+        
     return AIC_list, BIC_list, params, expectations
     
     
     
 
-def k_means_clustering(X_cleaned,K=10,maxiter=200):
+def k_means_clustering(X_cleaned,K=10,maxiter=100):
 
     # get dimensions of data
     n = X_cleaned.shape[0]
@@ -240,6 +270,23 @@ def k_means_clustering(X_cleaned,K=10,maxiter=200):
     return centroids, data_clustering
 
 
+def produce_images():
+    img = cv2.imread("/Users/Omar/Documents/Year4/machineLearning/coursework2/" + 
+             "data/question1/FluorescentCells.jpg")
+    img_cleaned = clean_data(img)
+    ## k means
+    k_values = [5,3,2]
+    kmeans_images = np.zeros((len(k_values),1927,2560,3))
+    gmm_images = np.zeros((len(k_values),1927,2560,3))
+    
+    for i, k in enumerate(k_values):
+        centroids, data_clustering = k_means_clustering(img_cleaned,K=k)
+        kmeans_images[i] = centroids[data_clustering].reshape((1927,2560,3))/float(255)
+        plt.imsave("kmeans_" + str(k),kmeans_images[i])
+        parameters, expectations = run_GMM(img_cleaned,K=k,maxiter=40)
+        gmm_images[i] = parameters["means"][np.argmax(expectations,axis=1)].reshape(1927,2560,3)/float(255)
+        plt.imsave("gmm_" + str(k),gmm_images[i])
+        
 
 
 
@@ -251,37 +298,38 @@ if __name__ == "__main__":
                  "data/question1/FluorescentCells.jpg")
     img_cleaned = clean_data(img)
     
-    parameters, expectations = run_GMM(img_cleaned,K=3)
-    myimg_gmm = parameters["means"][np.argmax(expectations,axis=1)]
+    produce_images()
     
-    cell_colour = np.array([ 155.43654699,  175.94150354,   93.08141005])    
-    myimg_gmm_blackwhite = np.where(np.isclose(myimg_gmm,cell_colour).all(axis=1),1,0).reshape((1927,2560))
-    
-    cleaned_dataset = np.column_stack(((np.where(myimg_gmm_blackwhite == 1))[0],np.where(myimg_gmm_blackwhite == 1)[1]))    
-    
-    AICS, BICS, params, expectations = count_cells(cleaned_dataset,82,90)
-    
-    plt.scatter(cleaned_dataset[:,0],cleaned_dataset[:,1],s=0.1)
-    plt.scatter(centroids[:,0],centroids[:,1],color="red")
-    
-    
-    
-    ### ERRORS:
-    ## I've chosen all the NON cells, not the actual cells
-    ## I think column stack doesn't work properly
-    
-#    centroids, data_clustering = k_means_clustering(img_cleaned,K=4)
-#    myimg_kmeans = centroids[data_clustering].reshape((1927,2560,3))/float(255)
+#    parameters, expectations = run_GMM(img_cleaned,K=3)
+#    myimg_gmm = parameters["means"][np.argmax(expectations,axis=1)]
+#    
+#    cell_colour = np.array([ 155.43654699,  175.94150354,   93.08141005])    
+#    myimg_gmm_blackwhite = np.where(np.isclose(myimg_gmm,cell_colour).all(axis=1),1,0).reshape((1927,2560))
+#    
+#    cleaned_dataset = np.column_stack(((np.where(myimg_gmm_blackwhite == 1))[0],np.where(myimg_gmm_blackwhite == 1)[1]))    
+#    
+#    AICS, BICS, params, expectations = count_cells(cleaned_dataset,82,90)
+#    
+#    plt.scatter(cleaned_dataset[:,0],cleaned_dataset[:,1],s=0.1)
+#    plt.scatter(centroids[:,0],centroids[:,1],color="red")
 #    
 #    
 #    
-#    myimg_gmm = parameters["means"][np.argmax(expectations,axis=1)].reshape((1927,2560,3))
-#    cv2.imwrite('kmeans4test.jpeg',myimg_kmeans*255)
-#    
-
-#    cv2.imwrite('gmm32.blackwhite.jpg',myimg_gmm_blackwhite*255)
-#    
-
-#    
+#    ### ERRORS:
+#    ## I've chosen all the NON cells, not the actual cells
+#    ## I think column stack doesn't work properly
 #    
 #
+##    
+##    
+##    
+##    myimg_gmm = parameters["means"][np.argmax(expectations,axis=1)].reshape((1927,2560,3))
+##    cv2.imwrite('kmeans4test.jpeg',myimg_kmeans*255)
+##    
+#
+##    cv2.imwrite('gmm32.blackwhite.jpg',myimg_gmm_blackwhite*255)
+##    
+#
+##    
+##    
+##
